be308f8fee0244e701dc4a82a0bca9347ee4b130
Josh Rehanek <joshrehanek@gmail.com>
2021-04-02 10:59:07 -0700

    deploy2.0

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 const PORT = 3000;
 
 const app = express();
@@ -14,17 +24,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
 
 // routes
 app.use(require("./routes/api.js"));
